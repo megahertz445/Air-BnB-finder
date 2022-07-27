e1531cf09069ea7032f1c21360c50d4fd5ef05c2
Ela Murat <ela.murat@icloud.com>
2022-07-27 13:51:48 -0500

    Initial Commit

---
+++ Air BnB finder/Air_BnB_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_BnB_finderApp.swift
+//  Air BnB finder
+//
+//  Created by Ela Murat on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Air_BnB_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Air BnB finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Air BnB finder
+//
+//  Created by Ela Murat on 27/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
